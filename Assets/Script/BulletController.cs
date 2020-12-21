70cba7a5e4979e73fcc298def0f09fbf40ba3407
hakuyou00 <kurokage.hi@gmail.com>
2020-12-22 02:01:56 +0900

    tank add

---
+++ Assets/Script/BulletController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private float time;
+    private bool onGround = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //砲弾のy座標が0.55以下になって3秒経過したら消滅
+        if(!onGround && this.transform.position.y <= 0.55f)
+        {
+            time = Time.time;
+            onGround = true;
+        }
+        if(onGround && (Time.time - time) >= 3f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
