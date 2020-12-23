6b1ad92a9db9b0c0270c8f9bd2f204b8444af935
hakuyou00 <kurokage.hi@gmail.com>
2020-12-24 02:29:16 +0900

    create mini

---
+++ Assets/Script/MiniDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniDoor : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
